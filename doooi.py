7d848b07a8911311a38cf44b96ea3b940617f897
Mohamed-Mostafa69 <Mohammed.Mostafaa69@gmail.com>
2023-09-22 21:55:16 +0300

    third com

---
+++ doooi.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands, tasks
+
+intents = discord.Intents(guilds=True, members=True)
+intents.presences = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="&", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Genshin Impact Players"))
+    print(f'{bot.user} has connected to Discord!')
+    check_genshin_players.start()
+
+
+@tasks.loop(seconds=60)
+async def check_genshin_players():
+    for guild in bot.guilds:
+        for member in guild.members:
+            if member.bot:
+                continue
+
+            for activity in member.activities:
+                if isinstance(activity, discord.Game) and activity.name == 'Genshin Impact':
+                    try:
+                        await member.kick(reason="Playing Genshin Impact")
+                        await member.send(
+                            "You were kicked from the server for playing Genshin Impact."
+                        )
+
+                        print(f"Kicked {member} for playing Genshin Impact.")
+                    except discord.Forbidden:
+                        print(
+                            f"Failed to kick {member}. Insufficient permissions.")
+                    except discord.HTTPException as e:
+                        print(f"Failed to kick {member}. Error: {e}")
+
+
+bot.run("MTAxNDI1NDM1MjE4MzA4MzEyOA.GX5mHV.4VoK70luj1kOTfn1S90p9SYRvAg7nyuQffQ44A")
